efb2e7d09a8d47dc7e7204bf45c1ea98027bd351
Noureldin Baraa Abdelhafez <xnourxb75@gmail.com>
2024-05-12 00:10:41 +0300

    ddl

---
+++ SQL/DDL.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Cars (
+    CarID INT AUTO_INCREMENT PRIMARY KEY,
+    Model VARCHAR(255) NOT NULL,
+    Year_ INT NOT NULL,
+    PlateID VARCHAR(50) UNIQUE NOT NULL,
+    Status_ VARCHAR(50) NOT NULL,
+    BaseRate DECIMAL(10, 2) NOT NULL,
+    LocationID INT,
+    FOREIGN KEY (LocationID) REFERENCES Locations(LocationID)
+);
+
+CREATE TABLE Customers (
+    CustomerID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) UNIQUE NOT NULL,
+    Username VARCHAR(20) UNIQUE NOT NULL
+);
+
+CREATE TABLE Reservations (
+    ReservationID INT AUTO_INCREMENT PRIMARY KEY,
+    CarID INT NOT NULL,
+    CustomerID INT NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE NOT NULL,
+    PickupLocationID INT NOT NULL,
+    DropOffLocationID INT NOT NULL,
+    Status_ VARCHAR(100) NOT NULL,
+    FOREIGN KEY (CarID) REFERENCES Cars(CarID),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY (PickupLocationID) REFERENCES Locations(LocationID),
+    FOREIGN KEY (DropOffLocationID) REFERENCES Locations(LocationID)
+);
+
+CREATE TABLE Payments (
+    PaymentID INT AUTO_INCREMENT PRIMARY KEY,
+    ReservationID INT NOT NULL,
+    Amount DECIMAL(10, 2) NOT NULL,
+    PaymentDate DATE NOT NULL,
+    PaymentMethod VARCHAR(100) NOT NULL,
+    FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
+);
+
+CREATE TABLE Locations (
+    LocationID INT AUTO_INCREMENT PRIMARY KEY,
+    LocationName VARCHAR(255) NOT NULL,
+    Country VARCHAR(100) NOT NULL,
+    City VARCHAR(100) NOT NULL,
+);
